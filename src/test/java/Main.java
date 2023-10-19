6b7621322f703fa6a37539210dc6d4b790f9a417
PimSc <pimschuitema@gmail.com>
2023-10-19 11:17:47 +0200

    naam-geven

---
+++ src/test/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+Person person = new Person("Reza","Achternaam",22, 'm');
+        System.out.println(person);
+
+
+
+    }
+}
+
